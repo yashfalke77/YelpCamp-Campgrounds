2a17a5be8f963d248fadccc0aa2baa41c3de5587
Yash Falke <yashfalke77@gmail.com>
2021-09-13 14:39:54 +0530

    Error Handling

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const path = require('path');
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 const methodOverride = require('method-override')
-const Campground = require('./models/campgrounds');  
+const Campground = require('./models/campgrounds');
 const ejsMate = require('ejs-mate');  // Ejs mate is used for more functionality of ejs partials
-
 // Ejs mate is used for more functionality of ejs partials
-
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+const Joi = require('joi')  //use for validations by defining schema
+const {campgroundSchema} = require('./joiSchemas')
 const app = express();
+
+
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        // Since details is array of object
+        const msg = error.details.map(el => el.message).join(', ')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
 
 app.engine('ejs', ejsMate);   //use ejs mate instead of ejs
 app.set('view engine', 'ejs');
@@ -21,7 +36,7 @@ db.once('open', () => {
 });
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
@@ -29,45 +44,57 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-});
+    res.render('campgrounds/index', { campgrounds })
+}));
 
-app.get('/campgrounds/new', async(req,res) => {
+app.get('/campgrounds/new', catchAsync(async (req, res) => {
     res.render('campgrounds/new')
-})
+}));
 
-app.post('/campgrounds', async(req,res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id }`);
-})
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params
     const campground = await Campground.findById(id)
-    res.render('campgrounds/show', {campground})
-});
+    res.render('campgrounds/show', { campground })
+}));
 
 
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const {id} = req.params
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+    const { id } = req.params
     const campground = await Campground.findById(id)
-    res.render('campgrounds/edit', {campground})
-})
+    res.render('campgrounds/edit', { campground })
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true, runValidators: true})
-    res.redirect(`/campgrounds/${campground._id }`);
-})
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true, runValidators: true })
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
+app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect(`/campgrounds`)
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not Found', 404))
+})
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).render('error', { err });
+
 })
 
 app.listen('8080', () => {
